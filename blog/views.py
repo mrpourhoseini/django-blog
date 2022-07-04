9578565e1f8db823d164217951e85222312be25c
Mohammadreza Pourhoseini <mohammadreza.pour.computer@gmail.com>
2022-07-04 21:04:45 +0430

    Add view and templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from blog.models import Post
+
 
 # Create your views here.
+def home(request):
+    posts = Post.objects.filter(status=Post.StatusChoices.PUBLISHED)
+    context = {
+        'posts': posts,
+    }
+    return render(request, "pages/index.html", context)
+
+
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    context = {
+        'post': post,
+    }
+    return render(request, "pages/detail.html", context)
